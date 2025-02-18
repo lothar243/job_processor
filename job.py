b64b1e22607194817a7aa3edcb3ecc4ab217f0fa
Jeff Arends <jeffrey.arends@mso.umt.edu>
2025-02-18 19:54:29 +0000

    Added requirements.txt and ability to specify download locationwq

--- job.py
+++ job.py
@@ -7,11 +7,12 @@ load_dotenv()
 
 URL = os.getenv("TARGET_URL", "http://google.com")
 LOG_FILE = os.getenv("LOG_FILE", "/home/jeff/job_processor/job.log")
+DOWNLOAD_LOCATION = os.getenv("DOWNLOAD_LOCATION", "/home/jeff/job_processor/pages")
 
 def download_page():
     response = requests.get(URL)
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    file_path = f"/home/jeff/job_processor/pages/page_{timestamp}.html"
+    file_path = f"{DOWNLOAD_LOCATION}/page_{timestamp}.html"
 
     with open(file_path, "w", encoding="utf-8") as file:
         file.write(response.text)
